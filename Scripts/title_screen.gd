b50b11e34994973936dad3b40fc12d0d734f0b69
Mar-iiy <mariana.cnascimento17@gmail.com>
2026-02-25 18:41:45 -0300

    INICIO TILEMAP

--- Scripts/title_screen.gd
+++ Scripts/title_screen.gd
@@ -1,1 +1,11 @@
 extends ColorRect
+
+
+func _on_play_button_down() -> void:
+	get_tree().change_scene_to_file("res://scenes/Gameplay/gameplay_1.tscn")
+
+func _on_quit_button_down() -> void:
+	get_tree().quit()
+
+func _on_options_button_down() -> void:
+	get_tree().change_scene_to_file("res://scenes/TELAS/Options.tscn")
