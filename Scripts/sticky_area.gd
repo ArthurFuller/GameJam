161557acb2a7046c36176e14391c8f7646691e6d
Mar-iiy <mariana.cnascimento17@gmail.com>
2026-02-26 19:03:17 -0300

    TELA INICIAL E LEVEL 1

--- Scripts/sticky_area.gd
+++ Scripts/sticky_area.gd
@@ -5,28 +5,29 @@ var original_color:Color
 
 @onready var game = get_node("/root/Game")
 
+@onready var sprite: AnimatedSprite2D = $AnimatedSprite2D
+
 func _ready() -> void:
 	monitoring = false
 	original_color = modulate
-	
 
 func _process(delta: float) -> void:
 	pass
 
-	
 func activate():
 	print("MONITORING LIGADO")
 	monitoring = true
 	is_sticky = true
 	modulate = Color(0.0, 10.55, 0.0, 1.0)
 	game.points -= 1
-	
+	update_animation()
 
 func deactivate():
 	monitoring = false
 	is_sticky = false
 	modulate = original_color
 	game.points += 1
+	update_animation()
 
 func _on_body_entered(body: Node2D) -> void:
 	print ("Entrou:", body.name)
@@ -34,8 +35,23 @@ func _on_body_entered(body: Node2D) -> void:
 		print("ATIVANDO WALL JUMP")
 		body.can_wall_jump = true
 
+
 func _on_body_exited(body: Node2D) -> void:
 		if body.is_in_group("Player"):
 			body.can_wall_jump = false
 			if  "is_wall_jumping" in body:
 				body.is_wall_jumping = false
+				
+
+func update_animation():
+	
+	var previous_animation = sprite.animation
+	var new_animation: String
+	
+	if is_sticky == true:
+		new_animation = "carimbo"
+	else:
+			new_animation = "nada"
+	if sprite.animation != new_animation:
+		sprite.play(new_animation)
+	
--- Scripts/title_screen.gd
+++ Scripts/title_screen.gd
@@ -1,11 +1,10 @@
 extends ColorRect
 
-
-func _on_play_button_down() -> void:
+func _on_play_button_button_down() -> void:
 	get_tree().change_scene_to_file("res://scenes/Gameplay/gameplay_1.tscn")
 
-func _on_quit_button_down() -> void:
-	get_tree().quit()
-
-func _on_options_button_down() -> void:
+func _on_credits_button_button_down() -> void:
 	get_tree().change_scene_to_file("res://scenes/TELAS/Options.tscn")
+	
+func _on_quit_button_button_down() -> void:
+	get_tree().quit
